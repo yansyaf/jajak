ce7aa06ba938fc6fad5b8ae3a131ced1f72efe3e
artikow <artikow@gmail.com>
2017-02-18 15:57:03 +0700

    cors http configuration fix

--- utils/cors.go
+++ utils/cors.go
@@ -4,6 +4,6 @@ import "github.com/rs/cors"
 
 func EnableCors() *cors.Cors {
 	return cors.New(cors.Options{
-		AllowedOrigins: []string{"http://128.199.91.72", "http://127.0.0.1"},
+		AllowedOrigins: []string{"http://12*"},
 	})
 }
